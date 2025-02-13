f7736d1693541ea3f8418ad3eb196c586f0402e3
cvissiaICS <cvissia@icstars.org>
2025-02-13 15:06:22 -0600

    First Push
    Trying to add a future function for displaying the specific numbers so user can be reminded of already guessed numbers.

---
+++ Number Guessing Game/guessing.js
@@ -0,0 +1,16 @@
+const targetNum = Math.floor(Math.random() * 100) + 1;
+let attempts = 0;
+
+function checkGuess() {
+    const userGuess = parseInt(document.getElementById("userGuess").value);
+    attempts++
+    if (userGuess == targetNum) {
+        document.getElementById("message").innerHTML = 'Correct! The number$(targetNum) in $(attempts)';
+    }
+    else if (userGuess > targetNum) {
+        document.getElementById("message").innerHTML = 'Reaching too high for that number.';
+    }
+    else {
+        document.getElementById("message").innerHTML = 'Not shooting high enough. Aim Higher';
+    }
+}
