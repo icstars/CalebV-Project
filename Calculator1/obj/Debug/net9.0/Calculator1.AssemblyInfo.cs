6d213182519dd4086b3d9c67b5835751ee3467f1
cvissiaICS <cvissia@icstars.org>
2025-03-03 14:22:43 -0600

    Branch Fix
    Python Calculator was not in correct folder

--- Calculator1/obj/Debug/net9.0/Calculator1.AssemblyInfo.cs
+++ Calculator1/obj/Debug/net9.0/Calculator1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1858488c967ebc3798bffc831b4c0ff2095fcae1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
